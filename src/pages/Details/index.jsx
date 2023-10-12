e36c138985cffaa2908f74975d1519ba77f787cf
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-10-12 12:12:03 -0300

    Installing the 'react-icons' library and creating and implementing the 'Section' and 'Tag' components
    Installing the 'react-icons' library to work with icons in the application, using a 'Logout' icon in the 'Header' component. Creating the 'Section' component to represent a section on the 'Details' page and creating the 'Tag' component to represent a content marker for a note. Finally, applying the styles of these components through their respective 'style.js' files and implementing them in the 'Details' page component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,28 @@
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
-import { Container } from './styles'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+import { Container, Links } from './styles'
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
       <Button>Voltar</Button>
     </Container>
   )

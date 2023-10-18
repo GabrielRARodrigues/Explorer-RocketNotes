2b98122911796be111ba9671e9cd170a461eb284
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-10-18 14:23:11 -0300

    Creating the 'NoteItem' component and finalizing the 'New' page
    Creating the 'NoteItem' component representing a custom input field. Adding the last two sections to the 'New' page component and the button to submit the form. Finally, applying the 'NoteItem' component to the 'New' page and completing the page's construction by adding the final component styles

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,9 @@ import { Container, Form } from './styles'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { TextArea } from '../../components/TextArea'
+import { Section } from '../../components/Section'
+import { NoteItem } from '../../components/NoteItem'
+import { Button } from '../../components/Button'
 
 export function New() {
   return (
@@ -19,8 +22,21 @@ export function New() {
           <Input placeholder="Título" />
 
           <TextArea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://www.rocketseat.com.br/" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Novo marcador" />
+            </div>
+          </Section>
+
+          <Button>Salvar</Button>
         </Form>
-        
       </main>
     </Container>
   )
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -9,6 +9,17 @@ export const Container = styled.div`
   grid-template-areas:
     'header'
     'content';
+
+  & > main {
+    grid-area: content;
+    overflow-y: auto;
+  }
+
+  .tags {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 `
 
 export const Form = styled.form`
@@ -35,5 +46,9 @@ export const Form = styled.form`
       line-height: 26px;
       color: ${({ theme }) => theme.COLORS.GRAY_100};
     }
+  }
+
+  & > section:first-of-type {
+    margin-bottom: 48px;
   }
 `

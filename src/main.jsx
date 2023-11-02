a2e6ed71556bc975f3d9095ecc5adbdfabad0427
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-11-02 15:42:55 -0300

    Creating the 'AuthContext' context in place of 'MyContext' and developing a custom hook responsible for user authentication.
    Creating the 'hooks' folder to store the custom hooks present in the application, creating the user authentication hook, and replacing the 'MyContext' context with the 'AuthContext' context that stores user authentication information

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 
-import { MyContext } from './myContext'
+import { AuthProvider } from './hooks/auth'
 
 import { Routes } from './routes'
 
@@ -13,11 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <MyContext.Provider
-        value={{ name: 'Gabriel Rodrigues', email: 'gabriel@email.com' }}
-      >
+      <AuthProvider>
         <Routes />
-      </MyContext.Provider>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,15 +1,24 @@
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
-import { FiMail, FiLock } from 'react-icons/fi'
+import { useState } from 'react'
 
 import { Container, Form, Background } from './styles'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { FiMail, FiLock } from 'react-icons/fi'
+
 import { Link } from 'react-router-dom'
-import { useContext } from 'react'
-import { MyContext } from '../../myContext'
+
+import { useAuth } from '../../hooks/auth'
 
 export function SignIn() {
-  const data = useContext(MyContext)
-  console.log('MEU CONTEXTO =>', data)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
 
   return (
     <Container>
@@ -19,11 +28,23 @@ export function SignIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input
+          value={email}
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={event => setEmail(event.target.value)}
+        />
 
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          value={password}
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={event => setPassword(event.target.value)}
+        />
 
-        <Button>Entrar</Button>
+        <Button onClick={handleSignIn}>Entrar</Button>
 
         <Link to="/register">Criar Conta</Link>
       </Form>

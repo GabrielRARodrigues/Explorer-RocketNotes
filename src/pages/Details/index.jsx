19107dafec1db33e756211c463265d81f57786ea
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-11-08 09:10:59 -0300

    Loading the user's notes on the home page, filtering the notes by tags and by the note's title, and displaying the note information in the 'Details' component
    Developing the logic for the 'Home' component of the homepage. Loading a list of user notes through the 'Note' component, fetching the user's notes stored in the database via the API, and storing this data in a state. Creating a state to capture the information about which tags are selected on the homepage and filtering only the notes that have these tags. Additionally, creating a state for the input field to filter the notes by the title entered in the field. Using the 'useEffect' hook to dynamically render routes based on the applied filters. Finally, when the user clicks on a note on the homepage, redirecting them to the note details page using the 'Details' component. The 'Details' component receives the ID of the note clicked by the user and fetches the data of that particular note to display it on the screen.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,49 +1,71 @@
+import { useEffect, useState } from 'react'
+
+import { Container, Content, Links } from './styles'
+
 import { Button } from '../../components/Button'
 import { ButtonText } from '../../components/ButtonText'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
-import { Container, Content, Links } from './styles'
+
+import { useNavigate, useParams } from 'react-router-dom'
+
+import { api } from '../../services/api'
 
 export function Details() {
+  const [data, setData] = useState(null)
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate('/')
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
   return (
     <Container>
       <Header />
-      <main>
-        <Content>
-          <ButtonText title="Excluir a nota" isActive />
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title="Excluir a nota" isActive />
 
-          <h1>Introdução ao React</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
 
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">https://www.rocketseat.com.br/</a>
-              </li>
-              <li>
-                <a href="#">https://www.rocketseat.com.br/</a>
-              </li>
-            </Links>
-          </Section>
+            {data.links && (
+              <Section title="Links úteis">
+                <Links>
+                  {data.links.map(link => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
 
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="nodejs" />
-          </Section>
-          <Button>Voltar</Button>
-        </Content>
-      </main>
+            {data.tags && (
+              <Section title="Marcadores">
+                {data.tags.map(tag => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
+            <Button onClick={handleBack}>Voltar</Button>
+          </Content>
+        </main>
+      )}
     </Container>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,10 +10,36 @@ import { Input } from '../../components/Input'
 import { Section } from '../../components/Section'
 import { Note } from '../../components/Note'
 
+import { useNavigate } from 'react-router-dom'
+
 import { api } from '../../services/api'
 
 export function Home() {
+  const [search, setSearch] = useState('')
   const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+  const [notes, setNotes] = useState([])
+
+  const navigate = useNavigate()
+
+  function handleTagSelected(tagName) {
+    if (tagName === 'all') {
+      return setTagsSelected([])
+    }
+
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if (alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+    } else {
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`)
+  }
 
   useEffect(() => {
     async function fetchTags() {
@@ -24,6 +50,17 @@ export function Home() {
     fetchTags()
   }, [])
 
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(
+        `/notes?title=${search}&tags=${tagsSelected}`
+      )
+      setNotes(response.data)
+    }
+
+    fetchNotes()
+  }, [tagsSelected, search])
+
   return (
     <Container>
       <Brand>
@@ -33,34 +70,41 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Todos" isActive></ButtonText>
+          <ButtonText
+            title="Todos"
+            isActive={tagsSelected.length === 0}
+            onClick={() => handleTagSelected('all')}
+          ></ButtonText>
         </li>
         {tags &&
           tags.map(tag => (
             <li key={String(tag.id)}>
-              <ButtonText title={tag.name}></ButtonText>
+              <ButtonText
+                title={tag.name}
+                isActive={tagsSelected.includes(tag.name)}
+                onClick={() => handleTagSelected(tag.name)}
+              ></ButtonText>
             </li>
           ))}
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" />
+        <Input
+          value={search}
+          placeholder="Pesquisar pelo título"
+          onChange={e => setSearch(e.target.value)}
+        />
       </Search>
 
       <Content>
         <Section title="Minhas notas">
-          <Note
-            data={{
-              title: 'React',
-              tags: [
-                { id: '1', name: 'react' },
-                {
-                  id: 2,
-                  name: 'nodejs'
-                }
-              ]
-            }}
-          />
+          {notes.map(note => (
+            <Note
+              key={String(note.id)}
+              data={note}
+              onClick={() => handleDetails(note.id)}
+            />
+          ))}
         </Section>
       </Content>
 

e04549cbe510715c8ed760302ce838b7098ab57f
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-11-09 11:11:30 -0300

    Finishing the application by adjusting the logout logic in the 'Header' component and handling non-existent routes.
    Finishing the development of the application. Adjusting the logout logic in the 'Header' component, creating the 'handleSignOut' function that uses the 'signOut' function from the authentication hook and sends the user to the root route ('/'), so that when the user logs out of the application, they are not redirected to a non-existent route. Finally, updating the application routes in the 'auth.routes.jsx' and 'app.routes.jsx' files, implementing a treatment to prevent the user from accessing a non-existent route and encountering a blank page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,11 +3,19 @@ import { Container, Logout, Profile } from './styles'
 import { RiShutDownLine } from 'react-icons/ri'
 import avatarPlaceholder from '../../assets/images/avatar_placeholder.svg'
 
+import { useNavigate } from 'react-router-dom'
+
 import { useAuth } from '../../hooks/auth'
 import { api } from '../../services/api'
 
 export function Header() {
   const { signOut, user } = useAuth()
+  const navigate = useNavigate()
+
+  function handleSignOut() {
+    navigate('/')
+    signOut()
+  }
 
   const avatarUrl = user.avatar
     ? `${api.defaults.baseURL}/files/${user.avatar}`
@@ -23,7 +31,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
